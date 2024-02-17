b6213d21beafdbf28c58ccd1c38d19a1a9f78168
xiajinghui_tz <xiajh1@chinatelecom.cn>
2024-02-17 16:40:55 +0800

    1

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
